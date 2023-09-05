fcbbaf30123491d3538b15aa7c5b3e698d822538
Litalhag <litalhag@gmail.com>
2023-09-05 23:32:15 +0300

    updated data

--- src/data.js
+++ src/data.js
@@ -2,27 +2,32 @@ const questions = [
   {
     id: 1,
     title: 'Do I have to allow the use of cookies?',
-    info: 'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    info: 'No, you do not have to allow the use of cookies on most websites. However, declining cookies might limit certain functionalities or user experiences on some sites. It is important to understand the purpose of the cookies being used and decide accordingly.',
   },
   {
     id: 2,
     title: 'How do I change my My Page password?',
-    info: 'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    info: 'go to the settings or account section of "My Page", find the "Change Password" option, and follow the prompts to enter your current and new passwords. Always ensure you save changes and use a strong, unique password for security.',
   },
   {
     id: 3,
     title: 'What is BankID?',
-    info: 'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+    info: 'BankID is a digital identity solution commonly used in several Nordic countries. It allows users to perform secure online transactions, sign documents, and authenticate their identity on various services, all facilitated by participating banks. Essentially, it is a digital form of identification comparable to traditional ID cards or passports, but backed by your bank.',
   },
   {
     id: 4,
-    title: 'Whose birth number can I use?',
-    info: 'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+    title: 'What is Two-Factor Authentication (2FA)',
+    info: 'Two-Factor Authentication (2FA) is an added layer of security for online accounts. Beyond just a username and password, 2FA requires a second piece of verification—often a code sent to a phone or email. This ensures that even if someone knows your password, they cannot access your account without the second factor.',
   },
   {
     id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info: 'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    title: 'What is the cloud in computing?',
+    info: 'The cloud, in computing, refers to storing and accessing data and programs over the internet instead of a computer/s local storage. It allows for on-demand access to a shared pool of resources, from applications to storage spaces, without directly managing the underlying infrastructure. ',
   },
-];
-export default questions;
+  {
+    id: 6,
+    title: 'How does a VPN protect online privacy?',
+    info: 'A VPN, or Virtual Private Network, encrypts a user/s internet connection, making their online activities private and untraceable. By routing data through a secure server, it hides the user/s IP address and location, safeguarding their identity from potential eavesdroppers or malicious actors.',
+  },
+]
+export default questions
